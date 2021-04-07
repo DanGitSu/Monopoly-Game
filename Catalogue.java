18f618cf7d4b46ec835b45c79001ee8b11405823
Daniel Su <sudaniel9@gmail.com>
2021-04-07 15:43:50 +1000

    test 1-3

--- Catalogue.java
+++ Catalogue.java
@@ -8,4 +8,34 @@ public class Catalogue {
 	private List<Genre> genres;
 
     		// write your solution here
+
+    Catalogue(Kiosk k){
+        this.kiosk = k;
+    }
+
+    public void use(){
+		System.out.println("Welcome to the Catalogue! Please make a selection from the menu:\n"
+        + "1. Display all games.\n"
+        + "2. Display all available games.\n"
+        + "3. Display all genres.\n"
+        + "4. Display games in a genre.\n"
+        + "5. Display all games by year.\n"
+        + "6. Rent a game.\n"
+        + "7. Return a game.\n"
+        + "R. Return to previous menu.");
+
+        System.out.print("Enter a choice: ");
+        String choice = In.nextLine();
+
+        if(choice.equals("1"));
+        else if(choice.equals("2"));
+        else if(choice.equals("3"));
+        else if(choice.equals("4"));
+        else if(choice.equals("5"));
+        else if(choice.equals("6"));
+        else if(choice.equals("7"));
+        else if(choice.equals("R")){
+            kiosk.use();
+        }
+	}
 }
--- Kiosk.java
+++ Kiosk.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 
+//import org.graalvm.compiler.lir.aarch64.AArch64Move.StoreOp;
+
 public class Kiosk {
 	private Catalogue catalogue; 
 	private List<Customer> customers;
@@ -8,10 +10,70 @@ public class Kiosk {
 		// write your solution here
 
 	public static void main(String[] args) {
-
+        new Kiosk();
 	}
 
-	private use(){
-		
+    Kiosk(){
+        this.catalogue = new Catalogue(this);
+        use();
+        
+    }
+
+	public void use(){ // main menu
+		System.out.println("Welcome to the Game Kiosk! Please make a selection from the menu:\n"
+        + "1. Explore the catalogue.\n"
+        + "2. View your customer record.\n"
+        + "3. Show you favourite games.\n"
+        + "4. Top up account.\n"
+        + "5. Enter Admin Mode.\n"
+        + "X. Exit the system.");
+
+        System.out.print("Enter a choice: ");
+        String choice = In.nextLine();
+
+        if(choice.equals("1")){
+            catalogue.use();
+        }
+        else if(choice.equals("2")) custRecordView();
+        else if(choice.equals("3"));
+        else if(choice.equals("4"));
+        else if(choice.equals("5")) admin();
+        else if(choice.equals("X")){
+            System.out.println("Thank you for using the Game Kiosk, do visit us again.");
+        }
 	}
+
+    private void admin(){ // admin menu
+        System.out.println("Welcome to the administration menu:\n"
+        + "1. List all customers.\n"
+        + "2. Add a customer.\n"
+        + "3. Remove a customer.\n"
+        + "4. List all games.\n"
+        + "5. Add a game to the catalogue.\n"
+        + "6. Remove a game from the catalogue.\n"
+        + "R. Return to the previous menu.");
+
+        System.out.print("Enter a choice: ");
+        String choice = In.nextLine();
+
+        if(choice.equals("1")){
+        }
+        else if(choice.equals("2"));
+        else if(choice.equals("3"));
+        else if(choice.equals("4"));
+        else if(choice.equals("5"));
+        else if(choice.equals("6"));
+        else if(choice.equals("R")){
+            use();
+        }
+    }
+
+    
+    // used to view a particular customers record
+    private void custRecordView(){ // 2nd choice main menu 2. View your customer record 
+        System.out.print("Enter a customer record: ");
+        String in = In.nextLine();
+        if (customers.size() > )
+        customers.get(Integer.parseInt(in)-1);
+    }
 }
